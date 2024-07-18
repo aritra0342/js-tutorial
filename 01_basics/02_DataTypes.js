f3f8a20053e2afbd88529d4a3a01d59da5fe9c9d
aritra0342 <144580613+aritra0342@users.noreply.github.com>
2024-07-18 11:37:28 +0000

    data types

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treats all JS code as newer version
+// alert(3 + 3) // we are using Node Js not browser
+
+console.log(3 + 3); // Code readability should be high
+
+console.log("hui");
+let name = "huii";
+let age = 18;
+let isLoggedIn = false;
+let state;
+// number 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false 
+// Null => standalone value
+// undefined
+// symbol => unique   
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
