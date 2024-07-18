455e25dabfa48dfd840221e2628cbe73b0179a83
aritra0342 <144580613+aritra0342@users.noreply.github.com>
2024-07-18 10:32:05 +0000

    container config

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 64466
+let accountmail = "babahu27@gmail.com"
+var password = "siuu"
+accountcity = "jaipur"
+accountmail = "siuu@k.com"
+password = "huiii"
+accountcity = "Bengaluru"
+let accstate
+console.table([accountId,accountmail,password,accountcity,accstate])
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/ 
+
