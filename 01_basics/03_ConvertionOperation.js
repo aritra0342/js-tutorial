8c610db655b145b7955879a81c55a86b5294b766
aritra0342 <144580613+aritra0342@users.noreply.github.com>
2024-07-22 11:22:59 +0000

    Convertion

---
+++ 01_basics/03_ConvertionOperation.js
@@ -0,0 +1,24 @@
+let score = undefined
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+//console.log(typeof(valueInNumber))
+//console.log(valueInNumber)
+// "33" => 33
+// "3abc" => NaN
+// True => 1; False => 0
+
+let isLoggedIn = "aritra"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log (typeof(booleanisLoggedIn))
+//console.log(booleanisLoggedIn)
+// "1" => True; "0" => False
+// "" => False
+// "aritra" => True
+let someNo = 33
+let number = String(someNo)
+console.log(typeof(number))
+console.log(number)
