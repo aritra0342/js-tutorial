ca3a6be77113c88db08d8d90230aca70453846b4
aritra0342 <144580613+aritra0342@users.noreply.github.com>
2025-02-10 14:00:57 +0000

    convertion_operations

--- 01_basics/03_ConvertionOperation.js
+++ 01_basics/03_ConvertionOperation.js
@@ -22,3 +22,25 @@ let someNo = 33
 let number = String(someNo)
 console.log(typeof(number))
 console.log(number)
+//*****************operations*****************//
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+let str1 ="Aritra"
+let str2 = " Hui"
+let str3 = str1 + str2
+console.log(str3)
+
+//console.log("1"+2)      //12
+//console.log(1+"2")      //12
+//console.log("1"+"2")    //12
+//console.log(1+2+"2")    //32
+//console.log(+true)      //1
+//console.log(+"")        //0
+let num1,num2,num3
+num1=num2=num3=2+2
+let gamecounter =100
+//gamecounter++   => 101
+//++gamecounter   => 101 
+console.log(gamecounter)
