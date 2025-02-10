f2df0d43bdd70a503da2ee412662bb2a357d9a44
aritra0342 <144580613+aritra0342@users.noreply.github.com>
2025-02-10 16:40:44 +0000

    strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,17 @@
+const name = "aritra"
+const repocount = 50
+console.log(`my name is ${name} and my repocount is ${repocount}`)
+const gamename = new String ('aritra')
+console.log(gamename[0])
+console.log(gamename.length)
+console.log(gamename.charAt(2))
+const newString = gamename.substring(0,4)
+console.log(newString)
+const anotherstring = gamename.slice(-5,4)
+console.log(anotherstring)
+const newstringone = "   aritra   "
+console.log(newstringone)
+console.log(newstringone.trim())
+const newname = "siuuuuuuuu20siuuuuuuuu"
+console.log(newname.replace('20','-'))
+console.log(newname.includes('siuu'))
