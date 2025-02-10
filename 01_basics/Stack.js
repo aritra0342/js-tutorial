911e3cf082b9667048659198a2b2147d27ea3c03
aritra0342 <144580613+aritra0342@users.noreply.github.com>
2025-02-10 15:24:42 +0000

    stack and heap

---
+++ 01_basics/Stack.js
@@ -0,0 +1,16 @@
+// Stack => premitive
+// Heap  => Non-premitive
+let myYoutubename = "aritrahui"
+
+let anothername = myYoutubename
+anothername= "anuhui"
+console.log(myYoutubename)
+console.log(anothername)
+let user1 = {
+    email : "Aritra@google.com",
+    upi : "aritra@ybl"
+}
+let user2 = user1
+user2.email = "anu@gmail.com"
+console.log(user1.email)
+console.log(user2.email)
